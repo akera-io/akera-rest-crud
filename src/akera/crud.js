ef00d152a25cdb3506f4ad097232bd0aa0ffe9a3
Marian Edu <marian.edu@acorn.ro>
2016-08-12 10:52:02 +0300

    defined prototype methods for crud/metadata and update dependencies

--- src/akera/crud.js
+++ src/akera/crud.js
@@ -1,3 +1,24 @@
-/**
- * http://usejsdoc.org/
- */
+var akera = require('akera-api');
+var async = require('async');
+var rsvp = require('rsvp');
+
+function AkeraCrud(broker) {
+
+  this.broker = broker;
+}
+
+AkeraCrud.prototype.create = function(table, data) {
+
+};
+
+AkeraCrud.prototype.read = function(table, pk, filter, sort, top, offset) {
+
+};
+
+AkeraCrud.prototype.update = function(table, pk, data) {
+
+};
+
+AkeraCrud.prototype.destroy = function(table, pk) {
+
+};
