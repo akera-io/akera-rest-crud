d158a3ec6f2f8444996ee4baacfb7de2086a9be3
Marian Edu <marian.edu@acorn.ro>
2016-08-13 11:40:48 +0300

    complete meta-data tests

--- test/akera-metadata-test.js
+++ test/akera-metadata-test.js
@@ -10,18 +10,125 @@ var broker = {
 
 describe('Akera Metadata', function() {
 
+  it('should load table', function(done) {
+    this.timeout(10000);
+
+    meta.getTable(broker, 'sports2000', 'Customer').then(
+      function(info) {
+        try {
+          should(info).be.an.instanceOf(Object);
+          should(info.fields).have.properties([ 'CustNum', 'Name', 'City' ]);
+          should(info.indexes).have
+            .properties([ 'CustNum', 'Name', 'SalesRep' ]);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      }, done);
+
+  });
+
+  it('should load sport database', function(done) {
+    this.timeout(10000);
+
+    meta.getDatabase(broker, 'sports2000').then(function(db) {
+      try {
+        should(db).be.an.instanceOf(Object);
+        should(db.table).have.properties([ 'Customer', 'Order', 'Item' ]);
+        done();
+      } catch (err) {
+        done(err);
+      }
+    }, done);
+
+  });
+
+  it('should load sport database from cache', function(done) {
+    meta.getDatabase(broker, 'sports2000').then(function(db) {
+      try {
+        should(db).be.an.instanceOf(Object);
+        should(db.table).have.properties([ 'Customer', 'Order', 'Item' ]);
+        done();
+      } catch (err) {
+        done(err);
+      }
+    }, done);
+
+  });
+
+  it('should fully load sport database', function(done) {
+    this.timeout(10000);
+
+    meta.getDatabase(broker, 'sports2000', true).then(
+      function(db) {
+        try {
+          should(db).be.an.instanceOf(Object);
+          should(db.table).have.properties([ 'Customer', 'Order', 'Item' ]);
+          should(db.table.Customer.fields).have.properties([ 'CustNum', 'Name',
+            'City' ]);
+          should(db.table.Customer.indexes).have.properties([ 'CustNum',
+            'Name', 'SalesRep' ]);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      }, done);
+
+  });
+
   it('should load databases', function(done) {
     this.timeout(10000);
 
     meta.getDatabases(broker).then(function(dbs) {
-      should(dbs).be.an.instanceOf(Object);
-      should(dbs).have.properties([ 'sports2000' ]);
-      done();
-    })['catch'](function(err) {
-      console.log(err);
-      done(err);
-    });
+      try {
+        should(dbs).be.an.instanceOf(Object);
+        should(dbs).have.properties([ 'sports2000' ]);
+        done();
+      } catch (err) {
+        done(err);
+      }
+    }, done);
 
-  })
+  });
+
+  it('should fully load databases', function(done) {
+    this.timeout(10000);
+
+    meta.getDatabases(broker, true).then(
+      function(dbs) {
+        try {
+          should(dbs).be.an.instanceOf(Object);
+          should(dbs).have.properties([ 'sports2000' ]);
+          should(dbs.sports2000.table).have.properties([ 'Customer', 'Order',
+            'Item' ]);
+          should(dbs.sports2000.table.Customer.fields).have.properties([
+            'CustNum', 'Name', 'City' ]);
+          should(dbs.sports2000.table.Customer.indexes).have.properties([
+            'CustNum', 'Name', 'SalesRep' ]);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      }, done);
+
+  });
+
+  it('should load table from cache', function(done) {
+    this.timeout(10000);
+
+    meta.getTable(broker, 'sports2000', 'Customer').then(
+      function(info) {
+        try {
+          should(info).be.an.instanceOf(Object);
+          should(info.fields).have.properties([ 'CustNum', 'Name', 'City' ]);
+          should(info.indexes).have
+            .properties([ 'CustNum', 'Name', 'SalesRep' ]);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      }, done);
+
+  });
 
 });
--- test/jsdo-metadata-test.js
+++ test/jsdo-metadata-test.js
@@ -13,30 +13,97 @@ var broker = {
 
 describe('JSDO Metadata', function() {
 
-  it('should load database services', function(done) {
+  it('should load all database services', function(done) {
     this.timeout(10000);
 
-    jsdoMeta.getCatalog(null, null, true, broker).then(function(dbs) {
-      should(dbs).be.an.instanceOf(Object);
-      should(dbs).have.properties([ 'services' ]);
-      should(dbs.services[0]).have.properties['resources'];
-      dbs.services[0].resources.forEach(function(resource) {
-        should(resource).have.properties['name', 'path', 'displayName', 'schema'];
-        should(resource.schema).have.properties['type', 'properties'];
-        Object.keys(resource.schema).forEach(function(ds) {
-          should(ds.indexOf('ds')).equal(0);
-          Object.keys(resource.schema.properties).forEach(function(tt) {
-            should(tt.indexOf('tt')).equal(0);
+    jsdoMeta.getCatalog(null, null, true, broker).then(
+      function(dbs) {
+        try {
+          should(dbs).be.an.instanceOf(Object);
+          should(dbs).have.properties([ 'services' ]);
+          should(dbs.services[0]).have.properties['resources'];
+          dbs.services[0].resources.forEach(function(resource) {
+            should(resource).have.properties['name', 'path', 'displayName',
+              'schema'];
+            should(resource.schema).have.properties['type', 'properties'];
+            Object.keys(resource.schema.properties).forEach(
+              function(ds) {
+                should(ds.indexOf('ds')).equal(0);
+                Object.keys(resource.schema.properties[ds].properties).forEach(
+                  function(tt) {
+                    should(tt.indexOf('tt')).equal(0);
+                  });
+              });
           });
-        });
-      });
-      console.log(dbs);
-      done();
-    })['catch'](function(err) {
-      console.log(err);
-      done(err);
-    });
+
+          done();
+        } catch (err) {
+          done(err);
+        }
+      }, done);
 
   });
 
+  it('should load sports2000 service', function(done) {
+    this.timeout(10000);
+
+    jsdoMeta.getCatalog('sports2000', null, true, broker).then(
+      function(dbs) {
+        try {
+          should(dbs).be.an.instanceOf(Object);
+          should(dbs).have.properties([ 'services' ]);
+          should(dbs.services[0]).have.properties['resources'];
+          dbs.services[0].resources.forEach(function(resource) {
+            should(resource).have.properties['name', 'path', 'displayName',
+              'schema'];
+            should(resource.schema).have.properties['type', 'properties'];
+            Object.keys(resource.schema.properties).forEach(
+              function(ds) {
+                should(ds.indexOf('ds')).equal(0);
+                Object.keys(resource.schema.properties[ds].properties).forEach(
+                  function(tt) {
+                    should(tt.indexOf('tt')).equal(0);
+                  });
+              });
+          });
+
+          done();
+        } catch (err) {
+          done(err);
+        }
+      }, done);
+
+  });
+
+  
+  it('should load only customer service', function(done) {
+    this.timeout(10000);
+
+    jsdoMeta.getCatalog('sports2000', 'Customer', true, broker).then(
+      function(dbs) {
+        try {
+          should(dbs).be.an.instanceOf(Object);
+          should(dbs).have.properties([ 'services' ]);
+          should(dbs.services[0]).have.properties['resources'];
+          dbs.services[0].resources.forEach(function(resource) {
+            should(resource).have.properties['name', 'path', 'displayName',
+              'schema'];
+            should(resource.schema).have.properties['type', 'properties'];
+            Object.keys(resource.schema.properties).forEach(
+              function(ds) {
+                should(ds.indexOf('ds')).equal(0);
+                Object.keys(resource.schema.properties[ds].properties).forEach(
+                  function(tt) {
+                    should(tt.indexOf('tt')).equal(0);
+                  });
+              });
+          });
+
+          done();
+        } catch (err) {
+          done(err);
+        }
+      }, done);
+
+  });
 });
